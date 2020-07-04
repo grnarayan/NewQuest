d837e26943be22b30688eff3d8eea709f86cb438
Narayan, Roy <Roy.Narayan@au.harveynorman.com>
2020-07-04 20:59:17 +1000

    Work in progress

--- Roy/Nq/Model/CartLimit.php
+++ Roy/Nq/Model/CartLimit.php
@@ -6,9 +6,25 @@ use Roy\Nq\Api\CartLimitInterface;
 class CartLimit implements CartLimitInterface
 {
 
+    // XML path for Alphonso Store Product Limit
+    private const XML_PATH_ALPHONOSO_PRODUCT_LIMIT = 'alphonso_store_setting/general/productlimit';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * CartLimit constructor.
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
     public function getLimit()
     {
-
+        return $this->scopeConfig->getValue(self::XML_PATH_ALPHONOSO_PRODUCT_LIMIT);
     }
 }
 
